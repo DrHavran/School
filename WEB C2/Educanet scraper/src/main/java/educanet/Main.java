3b07282fe5d370a0ee3eebf8be9e2d110e6fbbdf
DrHavran <krysacy@gmail.com>
2025-12-07 16:40:32 +0100

    switched .txt to .csv

--- WEB C2/Educanet scraper/src/main/java/educanet/Main.java
+++ WEB C2/Educanet scraper/src/main/java/educanet/Main.java
@@ -11,28 +11,31 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FileWriter fw;
-        fw = new FileWriter("output.txt", true);
+        fw = new FileWriter("output.csv", true);
 
 
         WebDriver driver = new FirefoxDriver();
 
         driver.get("https://praha.educanet.cz/teachers/pedagogicky-sbor/");
 
+        fw.write("Name,Email");
+        fw.write("\n");
+
         for(WebElement e : driver.findElements(By.className("post-box-bellow"))) {
 
             WebElement nameDiv = e.findElement(By.cssSelector(".post-box-title-inner"));
             String name = nameDiv.findElement(By.tagName("a")).getText();
             System.out.println(name);
-            fw.write("Name: " + name + "\n");
+            name = name.replaceAll(",", " ");
 
             if(!e.findElements(By.cssSelector(".contact-mail")).isEmpty()){
                 WebElement emailDiv = e.findElement(By.cssSelector(".contact-mail"));
                 String email = emailDiv.findElement(By.tagName("a")).getText();
                 System.out.println(email);
-                fw.write("Email: " + email + "\n");
+                fw.write(name + "," + email);
             }else{
                 System.out.println("No email");
-                fw.write("No Email" + "\n");
+                fw.write(name + ",no email");
             }
 
             System.out.println();
