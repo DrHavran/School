4b9a6d34af17b21e3e7630611037b434d71f33a7
DrHavran <krysacy@gmail.com>
2026-02-24 15:28:24 +0100

    calc

---
+++ Other/sedlák-3.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal EnableDelayedExpansion
+:CALC
+
+set /P x=Enter x: 
+set /P operation=Operation: 
+set /P y=Enter y: 
+
+set check="false"
+
+for %%n in (%*) do (
+    if "!operation!"=="%%n" set check="true"
+)
+
+if %check% == "false" exit
+
+if "%operation%"=="+" set /A result=x+y
+if "%operation%"=="-" set /A result=x-y
+if "%operation%"=="*" set /A result=x*y
+if "%operation%"=="/" set /A result=x/y
+
+echo ---------------------
+echo Result: %result%
+echo ---------------------
+
+goto CALC
