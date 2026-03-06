56f3c58175e17fcc4bfe8a3cadb4cc9024b764b5
DrHavran <krysacy@gmail.com>
2026-03-06 20:52:32 +0100

    idk anymore, i fzcking give up

--- Projects/O-AUTH/client/index.js
+++ Projects/O-AUTH/client/index.js
@@ -1,6 +1,9 @@
-fetch("http://localhost:8000/")
-  .then(response => response.text())
-  .then(data => console.log(data));
+let correct = "https://media.istockphoto.com/id/1133442802/vector/green-checkmark-vector-illustration.jpg?s=612x612&w=0&k=20&c=NqyVOdwANKlbJNqbXjTvEp2wIZWUKbfUbRxm9ROPk6M="
+let incorrect = "https://clipart-library.com/new_gallery/51-515337_x-mark-png-png-download-transparent-background-red.png"
+
+window.onload = function() {
+  document.getElementById("check").src = incorrect
+}
 
 
 function askForToken(){
@@ -8,3 +11,8 @@ function askForToken(){
         method: "POST",
     }).then(response => response.text()).then(data => console.log(data))
 }
+
+
+fetch("http://localhost:8000/")
+  .then(response => response.text())
+  .then(data => console.log(data));
